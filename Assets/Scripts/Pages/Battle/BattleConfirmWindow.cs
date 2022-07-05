efb176fb137d0d4b2c13a2559563c60131458637
NeDenis999 <84654874+NeDenis999@users.noreply.github.com>
2022-07-05 15:58:02 +0700

    Убрал баг с кривой анимацией page

--- Assets/Scripts/Pages/Battle/BattleConfirmWindow.cs
+++ Assets/Scripts/Pages/Battle/BattleConfirmWindow.cs
@@ -41,8 +41,7 @@ public class BattleConfirmWindow : MonoBehaviour
     {
         _enemyDefCards = enemyDefCards;
         _amountEnemyDefValue = amountEnemyDefValue;
-        _sequence = DOTween.Sequence();
-        
+
         gameObject.SetActive(true);
         ShowSmooth();
     }
@@ -79,29 +78,33 @@ public class BattleConfirmWindow : MonoBehaviour
                 _dataSaveLoadService.IncreaseEnergy(25);
             }
         }
-
-        _sequence.Kill();
+        
         gameObject.SetActive(false);
     }
 
     public void HideSmooth()
     {
+        _sequence?.Kill();
+        _sequence = DOTween.Sequence();
+        
         _sequence
             .Insert(0, DOTween.To(() => _canvasGroup.alpha, x => _canvasGroup.alpha = x, 0, 0.3f))
             .Insert(0, transform.DOLocalMove(_startPosition + new Vector3(0, -120, 0), 0.3f))
-            .OnComplete(() =>
-            {
-                _sequence.Kill();
-                gameObject.SetActive(false);
-            });
+            .OnComplete(() => gameObject.SetActive(false));
     }
     
     private void ShowSmooth()
     {
+        _sequence?.Kill();
+        _sequence = DOTween.Sequence();
+        
         _canvasGroup.alpha = 0;
         transform.localPosition = _startPosition + new Vector3(0, 120, 0);
         _sequence
             .Insert(0, DOTween.To(() => _canvasGroup.alpha, x => _canvasGroup.alpha = x, 1, 0.6f))
             .Insert(0, transform.DOLocalMove(_startPosition, 0.5f));
     }
+    
+    private void OnApplicationQuit() => 
+        _sequence?.Kill();
 }
