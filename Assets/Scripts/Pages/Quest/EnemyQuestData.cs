856d90f4122998d00812bf46de6c88d0260cd6f9
NeDenis999 <84654874+NeDenis999@users.noreply.github.com>
2022-07-10 15:28:54 +0700

    Обновил квест

---
+++ Assets/Scripts/Pages/Quest/EnemyQuestData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Pages.Quest
+{
+    [Serializable]
+    public struct EnemyQuestData
+    {
+        public EnemyType EnemyType;
+        public Sprite View;
+
+        [Range(100, 1000)] 
+        public int MaxHealth;
+
+        [Range(25, 100)] 
+        public int Damage;
+
+        [Range(10, 1000)] 
+        public int Exp;
+    }
+}
